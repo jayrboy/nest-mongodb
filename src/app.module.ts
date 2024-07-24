f83445981acc193297360fd4f9e9fb09f5f93d11
jarboy <j.jayox_@hotmail.com>
2024-07-25 00:44:56 +0700

    add resource users and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './modules/products/products.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    ProductsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
